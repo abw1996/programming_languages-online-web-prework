7f8718ad23a4010bf1d35034e0b405679ebee1c5
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-28 23:11:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,8 @@ def reformat_languages(languages)
   new_language_hash = {}
   languages.each do |style, language|
     language.each do |lang, data|
-      binding.pry 
+      binding.pry
+      new_language_hash[lang] = data
     end 
   end 
 end
